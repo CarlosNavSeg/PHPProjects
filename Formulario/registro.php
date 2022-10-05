e02f668ae82bb340082d45ee986d20dd0c35d480
CarlosNavSeg <carlos.raco@hotmail.com>
2022-10-05 10:33:31 +0200

    Mejora en el formulario de registro e limpieza de instalación errónea.

--- Formulario/registro.php
+++ Formulario/registro.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-header('AddHandler application/x-httpd-php52.php');
+$nomusuario = trim($_POST['nombre']);
+$contraseña = trim($_POST['contraseña']);
+$email = trim($_POST['email']);
 $opciones = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
@@ -12,16 +14,21 @@ $pdo = new PDO(
     'sa',
 $opciones);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-if(strlen($_POST['nombre'] >= 1) && strlen($_POST['email'] >= 1)) {
-        $nomusuario = trim($_POST['nombre']);
-        $contraseña = trim($_POST['contraseña']);
-        $email = trim($_POST['email']);
+if(strlen($_POST['nombre'] >= 1) && strlen($_POST['email'] >= 1) && strlen($_POST['contraseña'] >= 1)) {
+    $sentencia = "SELECT * FROM contactos WHERE password = '$contraseña' and email = '$email'";
+    $result = $pdo->prepare($sentencia);
+    $result->execute();
+    $gsent = $result->fetch(PDO::FETCH_ASSOC);
+    if($gsent == null) {
+        $query = "INSERT INTO contactos (username,password,email) VALUES ('$nomusuario', '$contraseña', '$email')";
+        $resultado = $pdo->exec($query);
         echo('Te has inscrito correctamente');
+        header('Location:/Formulario/login.html');
+    }
+    else{
+        echo('Usuario registrado');
+    }
 }
 else {
     echo('Completa los campos');
 }
-$query = "INSERT INTO contactos (username,password,email) VALUES ('$nomusuario', '$contraseña', '$email')";
-$resultado = $pdo->exec($query);
-header('Location:/Formulario/login.html');
-?>
